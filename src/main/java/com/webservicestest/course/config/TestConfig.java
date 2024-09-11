7e299cfc1035363e8ae83680d3a83fd1ee25e8a0
Miguel Sabino <miguelsabino2008@gmail.com>
2024-09-11 01:48:18 -0300

    Payment, one-to-one association

--- src/main/java/com/webservicestest/course/config/TestConfig.java
+++ src/main/java/com/webservicestest/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.webservicestest.course.entities.Category;
 import com.webservicestest.course.entities.Order;
 import com.webservicestest.course.entities.OrderItem;
+import com.webservicestest.course.entities.Payment;
 import com.webservicestest.course.entities.Product;
 import com.webservicestest.course.entities.User;
 import com.webservicestest.course.entities.enums.OrderStatus;
@@ -81,5 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
