00a60e8f7b6dea4b1f4929a0a4f387f0840d9d2a
Miguel Sabino <miguelsabino2008@gmail.com>
2024-11-13 23:18:13 -0300

    Exception Handling - delete

--- src/main/java/com/webservicestest/course/services/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/webservicestest/course/services/exceptions/ResourceExceptionHandler.java
@@ -21,4 +21,12 @@ public class ResourceExceptionHandler {
 		StandardError er = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(er);
 	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database Error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError er = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(er);
+	}
 }
